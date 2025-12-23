90f49e10c3394d7e952718569c96148cc1ca4dc5
QiFeng Sun <13756232127@163.com>
2025-12-23 22:13:10 +0800

    Update WiFi credentials and API key placeholders

--- ESP01s.h
+++ ESP01s.h
@@ -7,16 +7,16 @@
 // =====================================================
 // 第1步：配置参数（请修改为你自己的）
 // =====================================================
-#define WIFI_SSID       "u_Do"           // 你的WiFi名称
-#define WIFI_PASSWORD   "13756232127"       // 你的WiFi密码
+#define WIFI_SSID       "xxx"           // 你的WiFi名称
+#define WIFI_PASSWORD   "xxx"           // 你的WiFi密码
 
 // 固定明文域名：mqtts.heclouds.com:1883（不启用TLS）
 #define ONENET_SERVER   "mqtts.heclouds.com"
 #define ONENET_PORT     1883
 
-#define PRODUCT_ID      "16DDGX7Ej6"        // 产品ID（控制台可见）
-#define DEVICE_NAME     "test"    // 设备名（创建设备的名称）
-#define API_KEY         "version=2018-10-31&res=products%2F16DDGX7Ej6%2Fdevices%2Ftest&et=1797330262&method=md5&sign=AXeu4WlB9gMxIqmlBn4wgw%3D%3D"  // 使用登录令牌(token)
+#define PRODUCT_ID      "xxx"        // 产品ID（控制台可见）
+#define DEVICE_NAME     "xxx"        // 设备名（创建设备的名称）
+#define API_KEY         "xxx"        // 使用登录令牌(token)
 // =====================================================
 
 // 定义网络初始化错误码
@@ -38,5 +38,6 @@ uint8_t ESP8266_MQTT_Sub(char *topic, int qos);
 uint8_t ESP8266_MQTT_PubRaw(char *topic, char *payload);
 
 uint8_t ESP8266_OneNet_Init(void);
+
 
 #endif /* __ESP01S_H__ */
